eaf8c3a918ac2622e427100dca4432830d977db0
Muraoka Naoki <naoki770915@gmail.com>
2021-02-03 21:46:07 +0900

    回転するスクリプト　回転の中心点と回転軸は自分で設定できる

---
+++ RotateScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScript : MonoBehaviour
+{
+    //public float rotx;
+    //public float roty;
+    //public float rotz;
+
+    [SerializeField] private Vector3 _center = Vector3.zero;
+    [SerializeField] private Vector3 _axis = Vector3.up;
+    [SerializeField] private float _period = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(
+            _center,
+            _axis,
+            360 /_period*Time.deltaTime
+            );
+    }
+}
