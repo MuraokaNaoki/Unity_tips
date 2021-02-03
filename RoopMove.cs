1b73412921e8d69e5bbed550c3d455da07e535ab
Muraoka Naoki <naoki770915@gmail.com>
2021-02-03 21:44:36 +0900

    四角形軌道で動くスプライト

---
+++ RoopMove.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Threading;
+
+public class RoopMove : MonoBehaviour
+{
+    public float startPosx;
+    public float startPosz;
+    public float endPosx;
+    public float endPosz;
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    bool m_xPlus = true;
+    bool m_zPlus = false;
+    bool m_x2Plus = false;
+    bool m_z2Plus = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_xPlus)
+        {
+            transform.position += new Vector3(moveSpeed * Time.deltaTime, 0f, 0f);
+            if(transform.position.x >= endPosx)
+            {
+                //Thread.Sleep(1000);
+                m_xPlus = false;
+                m_zPlus = true;
+            }
+        }
+
+        if (m_zPlus)
+        {
+            transform.position += new Vector3(0f, 0f, moveSpeed * Time.deltaTime);
+            if(transform.position.z >= endPosz)
+            {
+                //Thread.Sleep(1000);
+                m_zPlus = false;
+                m_x2Plus = true;
+            }
+        }
+
+        if (m_x2Plus)
+        {
+            transform.position += new Vector3(-moveSpeed * Time.deltaTime, 0f, 0f);
+            if(transform.position.x <= startPosx)
+            {
+                //Thread.Sleep(1000);
+                m_x2Plus = false;
+                m_z2Plus = true;
+            }
+        }
+
+        if (m_z2Plus)
+        {
+            transform.position += new Vector3(0f, 0f, -moveSpeed * Time.deltaTime);
+            if(transform.position.z <= startPosz)
+            {
+                //Thread.Sleep(1000);
+                m_z2Plus = false;
+                m_xPlus = true;
+            }
+        }
+    }
+}
