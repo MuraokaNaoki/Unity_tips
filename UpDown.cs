4d3d65e3ddeeddb8a19714b57877a75e4ddaffc6
Muraoka Naoki <naoki770915@gmail.com>
2021-02-03 21:35:21 +0900

    上下運動する踏み台

---
+++ UpDown.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpDown : MonoBehaviour
+{
+    Vector3 objPosition;
+    public float moveSpped;
+    // Start is called before the first frame update
+    void Start()
+    {
+        objPosition = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(objPosition.x, Mathf.Cos(Time.time)*moveSpped + objPosition.y, objPosition.z);
+    }
+}
